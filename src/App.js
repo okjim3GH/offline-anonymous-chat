abbe4a270df1d8d3ebc41c71d77664e50332117e
O'Keefe, James (ITS-CONTRACTOR) <James.OKeefe@its.ny.gov>
2018-01-29 10:46:46 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as RxDB from 'rxdb';
+import { QueryChangeDetector } from 'rxdb';
+import { schema } from './Schema';
+
+QueryChangeDetector.enable();
+QueryChangeDetector.enableDebugging();
+
+RxDB.plugin(require('pouchdb-adapter-idb'));
+RxDB.plugin(require('pouchdb-adapter-http'));
+
+const syncURL = 'http://localhost:5985/';
+const dbName = 'chatdb';
+
+class App extends Component {
+
+  async createDatabase() {
+    //
+    const db = await RxDB.create(
+      {name: dbName, adapter: 'idb', password: '12345678'}
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
